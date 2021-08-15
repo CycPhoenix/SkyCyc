0d7ae26e3391b11594ac14dc732df3750c863cc2
CycPhoenix <cycphoenixgt@gmail.com>
2021-08-15 15:40:56 +0800

    Create github.js

---
+++ commands/github.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const e = require('../embeds.json');
+
+module.exports = {
+    name: 'github',
+    description: 'DMs you the SkyCyc Github link',
+    cat: 'other',
+    guild: false,
+    alias: ['git', 'g'],
+    execute(message, args, client, prefix) {
+        const invite = new Discord.MessageEmbed()
+            .setDescription(
+                `${e.github} **Github Link**\n` +
+                `https://github.com/CycPhoenix/SkyCyc`
+            )
+
+        if (message.guild) {
+            message.author.send(invite).then(() => {
+                message.react('👌');
+            }).catch(() => {
+                const embed = new Discord.MessageEmbed()
+                    .setColor(e.red)
+                    .setDescription(`${e.x} **Please enable DMs from server members.**`);
+                message.channel.send(embed).then((newmsg) => {
+                    newmsg.delete({ timeout: 10000 });
+                });
+            });
+        } else {
+            message.author.send(invite).catch();
+        }
+    },
+};
