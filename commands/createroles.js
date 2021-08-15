d6e956f3a04a4a1564c93e16b70bcb6ccc9e7830
CycPhoenix <cycphoenixgt@gmail.com>
2021-08-15 15:39:03 +0800

    Create createroles.js

---
+++ commands/createroles.js
@@ -0,0 +1,73 @@
+const db = require('quick.db');
+const consola = require('consola');
+const Discord = require('discord.js');
+const e = require('../embeds.json');
+const ranks = require('../ranks.json');
+
+module.exports = {
+    name: 'createroles',
+    description: 'Creates default rank roles',
+    cat: 'settings',
+    guild: true,
+    alias: ['roles', 'role'],
+    async execute(message, args, client, prefix) {
+        if (!message.member.hasPermission('MANAGE_ROLES')) {
+            const embed = new Discord.MessageEmbed()
+                .setColor(e.red)
+                .setDescription(`${e.x} You need the **Manage Roles** permission to use this command`)
+            return message.channel.send(embed).then((newmsg) => {
+                newmsg.delete({ timeout: 5000 })
+            });
+        }
+
+        let waitembed = new Discord.MessageEmbed()
+            .setColor('YELLOW')
+            .setDescription(`${e.loading} **Please wait...**\nCreating missing roles`)
+
+        let newmsg = await message.channel.send(waitembed);
+        let tocreate = ['verified', 'VIP', 'VIP_PLUS', 'MVP', 'MVP_PLUS', 'SUPERSTAR'];
+
+        tocreate.push('YOUTUBER');
+        tocreate.push('GAME_MASTER');
+        tocreate.push('ADMIN');
+
+        tocreate.reverse();
+        let newroles = [];
+
+        for (let i = 0; i < tocreate.length; i++) {
+            let rankname = tocreate[i];
+            let rank = ranks[rankname];
+
+            let roleid = db.get(`${message.guild.id}.roles.${rankname}`);
+            if (roleid !== null && roleid !== undefined) {
+                if (message.guild.roles.cache.get(roleid) !== null && message.guild.roles.cache.get(roleid) !== undefined) {
+                    continue;
+                }
+            }
+
+            await message.guild.roles.create({
+                data: {
+                    name: rank.formatted,
+                    color: rank.color,
+                    permissions: [],
+                },
+                reason: `${message.author.tag}: Hypixel rank roles`
+            })
+            .then((role) => {
+                newroles.push(`<@&${role.id}>`);
+                db.set(`${message.guild.id}.roles.${rankname}`, role.id);
+            })
+            .catch(consola.error);
+        }
+
+        let desc = e.check + ' **Created the following roles**:\n' + ' ';
+
+        if (newroles.length < 1) desc += 'None';
+        else desc += newroles.join(', ');
+
+        let successembed = new Discord.MessageEmbed()
+            .setColor(e.green)
+            .setDescription(desc)
+        newmsg.edit(successembed);
+    },
+};
