ebf035f8dbe5e2166aae58e1ccbfdccaa4fe9420
CycPhoenix <cycphoenixgt@gmail.com>
2021-08-15 15:41:07 +0800

    Create db.js

---
+++ commands/db.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const db = require('quick.db');
+const owner = require('../owner.json');
+const e = require('../embeds.json');
+
+module.exports = {
+    name: 'db',
+    description: '',
+    cat: 'other',
+    guild: false,
+    alias: [],
+    execute(message, args, client, prefix) {
+        if (message.author.id !== owner.id)
+            return message.channel.send(':flushed:')
+                .then(newmsg => {
+                    newmsg.delete({
+                        timeout: 4000
+                    }).catch()
+                }).catch();
+
+        message.channel.send('here you go dummy', {
+            files: [
+                './json.sqlite'
+            ]
+        }).catch();
+    },
+};
